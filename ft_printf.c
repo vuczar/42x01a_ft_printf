4ab723506de8670939161a9186ea27fa8e4142c0
Vinicius Mauricio De Souza <vimauric@c3r13p1.42sp.org.br>
2026-08-05 17:40:43 -0300

    finally started

--- ft_printf.c
+++ ft_printf.c
@@ -1,22 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vimauric <vimauric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/03 11:35:06 by vimauric          #+#    #+#             */
-/*   Updated: 2026/08/05 17:35:05 by vimauric         ###   ########.fr       */
+/*   Created: 2026/08/05 17:32:56 by vimauric          #+#    #+#             */
+/*   Updated: 2026/08/05 17:39:53 by vimauric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <unistd.h>
-# include <stdarg.h>
-
-int	ft_printf(const char *format, ...);
-
-#endif
+int	ft_printf(const char *format, ...)
+{
+	va_list args;
+	int		total;
+	int		i;
+	
+	va_start(args, format);
+	if (!format)
+		return (-1);
+	i = 0;
+	total = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			
+		}
+		else
+			ft_putchar(format[i]);
+		i++;
+	}
+	return (total);
+}
