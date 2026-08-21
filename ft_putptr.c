55f4999f22ed7b34f90673ba98b1437a908c786a
noone <boringmkt@gmail.com>
2026-08-20 22:07:22 -0300

    finished ft_putptr

---
+++ ft_putptr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vimauric <vimauric@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/20 21:36:12 by vimauric          #+#    #+#             */
+/*   Updated: 2026/08/20 22:06:17 by vimauric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putptr(unsigned long n)
+{
+	char	*base;
+	int		count;
+
+	base = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+		count += ft_putptr(n / 16);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
